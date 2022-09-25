5a939a5538336047eddc70664e7b02565a2a5b11
vp22j <vp22jnp@gmail.com>
2022-09-25 13:22:05 +0530

    added: duplicate elements present in array

---
+++ com/bridgelabday3/duplicateElementsInarray.java
@@ -0,0 +1,17 @@
+package com.bridgelabday3;
+
+public class duplicateElementsInarray {
+        public static void main(String[] args) {
+            //Initialize array
+            int [] arr = new int [] {1, 2, 3, 4, 1, 7, 7, 8, 3};
+            System.out.println("Duplicate elements in given array: ");
+            //Searches for duplicate element
+            for(int i = 0; i < arr.length; i++) {
+                for(int j = i + 1; j < arr.length; j++) {
+                    if(arr[i] == arr[j])
+                        System.out.println(arr[j]);
+                }
+            }
+        }
+    }
+
