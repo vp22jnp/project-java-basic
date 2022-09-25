37ab47315ff709e820da998f60d0c3800825d939
vp22j <vp22jnp@gmail.com>
2022-09-25 13:18:14 +0530

    added: array present in odd position

---
+++ com/bridgelabday3/elementsOnOddPosition.java
@@ -0,0 +1,14 @@
+package com.bridgelabday3;
+
+public class elementsOnOddPosition {
+    public static void main(String[] args) {
+        int [] arr= new int[]{1,2,3,4,5};
+        System.out.println("enter arrays");
+        for(int i=0;i<arr.length;i=i+2){
+            System.out.println(arr[i]);
+
+        }
+    }
+}
+
+
