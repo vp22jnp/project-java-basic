25b964ea26dd2b9c24696b4863008695c3d0cdcd
vp22j <vp22jnp@gmail.com>
2022-09-25 11:49:42 +0530

    added:uc1 loop

---
+++ com/bridgelabday2/sumofNnaturalnumbers.java
@@ -0,0 +1,13 @@
+package com.bridgelabday2;
+
+public class sumofNnaturalnumbers {
+    public static void main(String[] args) {
+        int i,num=5,sum=0;
+        for(i=1;i<=num;++i){
+            sum=sum+i;
+        }
+        System.out.println("sumof natural numbers is :"+sum);
+
+    }
+}
+
